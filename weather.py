8cb317e2d7937926b5396cab23f3544f337ca6de
SchumacherFarad <kickmagical@gmail.com>
2024-07-15 19:36:37 +0300

    Bugfix
    Blocked the situation that user could enter a non integer type to days input.

--- weather.py
+++ weather.py
@@ -3,6 +3,26 @@ import json
 import requests
 from dotenv import load_dotenv, dotenv_values
 
+def can_be_int(inpt):
+    try:
+        int(inpt)
+        return True
+    except ValueError:
+        return False
+
+def get_valid_input():
+    while True:
+        usrinpt = input("How many days you want to see weather forecast for(1-10): ")
+        if can_be_int(usrinpt):
+            usrinpt = int(usrinpt)
+            if 1 <= usrinpt <=10:
+                return usrinpt
+            else:
+                print("You entered an unvalid input. Please enter a number from 1 to 10: ")
+        else:
+            print("You entered an unvalid input. Please enter a number from 1 to 10: ")
+        
+
 load_dotenv()
 
 KEY = os.getenv("API_KEY")
@@ -26,9 +46,7 @@ elif forc.upper() == "F":
     print("Enter a location to see the current temperature of it: ")
     loc = input("")
     request_url += "&q="+loc
-    days = int(input("How many days you want to see weather forecast for(1-10): "))
-    while days > 10 or days < 1 :
-        days = int(input("You entered an unvalid input. Please enter a number from 1 to 10: "))
+    days = get_valid_input()
     request_url +="&days=" + str(days) +"&aqi=no&alerts=no"
     response = requests.get(request_url)
     if response.status_code== 200:
@@ -36,4 +54,3 @@ elif forc.upper() == "F":
         print(str(days) + " Days Forecast of " + weather_data["location"]["name"] + ":")
         for i in range(days):
             print("Max and Min temperature in " + weather_data["location"]["name"] + ", "+ weather_data["location"]["region"]+ ", "+ weather_data["location"]["country"]+ " in " + weather_data["forecast"]["forecastday"][i]["date"]+ " are " + str(weather_data["forecast"]["forecastday"][i]["day"]["maxtemp_c"]) + "/" + str(weather_data["forecast"]["forecastday"][i]["day"]["mintemp_c"]))
-    
